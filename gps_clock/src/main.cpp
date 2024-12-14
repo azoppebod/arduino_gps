4aa2478b27c22555d1abd6eeb900d14d6fde8f26
azoppe <azoppelletto@blueorange.digital>
2024-12-14 00:55:48 -0300

    Update main.cpp
    Added a button on A2 and A3 to trigger the manual execution.
    Added Christmas and New Year Eve special schedules.

--- gps_clock/src/main.cpp
+++ gps_clock/src/main.cpp
@@ -6,7 +6,7 @@
   This code controls the turning on and off of a device at multiple times during the day using an Arduino Uno and a NEO-6M GPS module.
   The program retrieves the time from the GPS, converts it to the local time based on the specified time zone offset, and turns a relay 
   connected to a device on or off based on the current time. Multiple time windows during the day are defined for automatic light control.
-  It also shows the GPS state, time and schedule execution in a LCD.
+  It also shows the GPS state, time and schedule execution in a LCD. It has a button to trigger the manual execution.
   Libraries Used:
   - NeoSWSerial: For serial communication with the GPS module.
   - TinyGPS++: To decode data from the GPS module.
@@ -27,13 +27,14 @@
 // RX Arduino = TX GPS 
 // TX Arduino = RX GPS
 const int RXPin = 4, TXPin = 3;
-const int relayPin = 9, ledPin = 13, buttonPin = A1;
+const int relayPin = 9, ledPin = 13, buttonPin = A1, auxOverridePin = A2, overridePin = A3;
 const int PWMValue = 105;
 const uint32_t GPSBaud = 9600;
 const int offset = -3;
-bool fixStatus, turnOn;
+bool fixStatus, turnOn, manual;
 bool backlightState = false; // Variable to track backlight state
-int buttonState = 0, lastButtonState = 0;
+int buttonState = 0, lastButtonState = 0, activeSchedule;
+int overrideState = 0, lastOverrideState = 0;
 unsigned long lastTime = millis();
 
 // Creating objects
@@ -84,10 +85,40 @@ void handleButtonPress() {
   lastButtonState = buttonState;
 }
 
+void handleOverrideButtonPress() {
+  int analogValue = analogRead(overridePin);  // read button 
+  float voltageOverride = analogValue * (5.0 / 1023.0);  // convert into voltage
+
+  if (voltageOverride > 1) {
+    overrideState = 1;
+  } else {
+    overrideState = 0;
+  }
+  
+  if (overrideState != lastOverrideState) {
+    manual = !manual;
+
+    if (!turnOn) {
+      if (manual) {
+        digitalWrite(ledPin, HIGH);
+        analogWrite(relayPin, PWMValue);
+        writeToLCD(1, "ON: Manual");
+      } else {
+        digitalWrite(ledPin, LOW);
+        digitalWrite(relayPin, LOW);
+        writeToLCD(1, "OFF");
+      }
+    }
+  }
+
+  lastOverrideState = overrideState;
+}
+
 // GPS availability control
 bool checkGPSFix()
 {
   handleButtonPress(); // It checks if button was pushed while checking for GPS
+  handleOverrideButtonPress(); // It checks if override button was pushed while checking for GPS
   while ((neogps.available() > 0))
   {
     if (gps.encode(neogps.read()))
@@ -111,12 +142,12 @@ void initializeGPS() {
 
   lcd.backlight(); // Turn on backlight to see initialization messages
 
-  writeToLCD(0, "Buscando GPS");
+  writeToLCD(0, "Searching GPS");
   writeToLCD(1, "");
   while (!checkGPSFix()) {
     ;
   }
-  writeToLCD(0, "GPS encontrado");
+  writeToLCD(0, "GPS OK!");
   writeToLCD(1, "");
 
   delay(500);
@@ -136,8 +167,15 @@ void setup() {
   
   pinMode(buttonPin, INPUT_PULLUP);  // To set button as input
   analogWrite(buttonPin, LOW);  // No pressed as default
+
+  pinMode(auxOverridePin, OUTPUT);
+  digitalWrite(auxOverridePin, HIGH); // Sending voltage
+  pinMode(overridePin, INPUT_PULLUP); // To set override button as input
+  analogWrite(overridePin, LOW);  // No pressed as default
+
   pinMode(relayPin, OUTPUT); // To set relay as output
   digitalWrite(relayPin, LOW); // Turn off by default
+
   pinMode(ledPin, OUTPUT); // To set led as output
   digitalWrite(ledPin, LOW); // Turn off by default
 
@@ -147,12 +185,16 @@ void setup() {
 void loop() {
 
   handleButtonPress(); // Call to button press control
+  handleOverrideButtonPress(); // Call to override button press control
 
   while (neogps.available() > 0) {
     gps.encode(neogps.read());
 
     if (gps.time.isUpdated()) {
-      int hour = gps.time.hour(); // UTC current Time 
+      // UTC current Time 
+      int month = gps.date.month();
+      int day = gps.date.day();
+      int hour = gps.time.hour(); 
       int minute = gps.time.minute();      
       int second = gps.time.second();      
 
@@ -163,8 +205,8 @@ void loop() {
       }
 
       // Check if the current time is within any turn-on window
-      bool turnOn = false;
-      int activeSchedule = -1;
+      activeSchedule = -1;
+      turnOn = false;
 
       for (int i = 0; i < NUM_SCHEDULES; i++) {
         int startHour = schedules[i][0], startMinute = schedules[i][1], startSecond = schedules[i][2];
@@ -178,6 +220,12 @@ void loop() {
           activeSchedule = i + 1;
           break;
         }
+      }
+
+      if ((month == 12 && day == 25 && hour == 0 && minute == 0) || // Dec 25th
+          (month == 1 && day == 1 && hour == 0 && minute < 2)) { // Jan 1st
+        turnOn = true;
+        activeSchedule = (month == 12) ? 4 : 5; // 4 for Dec 25th, 5 for Jan 1st
       }
 
       // Create char arrays for time and satellites
@@ -195,11 +243,11 @@ void loop() {
       if (turnOn) {
         digitalWrite(ledPin, HIGH);
         analogWrite(relayPin, PWMValue);
-        writeToLCD(1, "Encendido: " + String(activeSchedule));
-      } else {
+        writeToLCD(1, "ON: " + String(activeSchedule));
+      } else if (!manual) {
         digitalWrite(ledPin, LOW);
         digitalWrite(relayPin, LOW);
-        writeToLCD(1, "Apagado");
+        writeToLCD(1, "OFF");
       }
     }
     lastTime = millis();
